5f863eb9b1a166ad39855735e7ffe0edaf09145a
shalawi007 <shoala1994@gmail.com>
2022-07-29 21:12:41 +0300

    sales and admin both insert are sepereated no sync

--- Dstr/Main.cpp
+++ Dstr/Main.cpp
@@ -6,320 +6,11 @@
 #include "Orderlist.h"
 
 using namespace::std;
+int main();
+void saleOrderExecutiveMenu();
+void administratorMenu();
 
-void saleOrderExecutiveMenu()
-{
-	int soeChoice, opt, noItem;
-	Orderlist list;
-	string id, itId, itRec, brand, price, orderDel;
-	char back;
-	cout << "1. Obtain purchase order records\n2. Modify/UpitemID order according to the priority\n3. View purchase orders\n4. Sort purchase orders\n5. Search purchase order\n6. Logout" << endl;
-	cin >> soeChoice;
-	if (soeChoice == 1)
-	{
-		do {
-			system("cls");
-			cout << "\n--------------------------------------------" << endl;
-			cout << "\n..................>>>>>>" << endl;
-			cout << "\nPurchase Order Records Obtained Succsefully..";
-			system("pause");
-			system("cls");
-			cout << "\n> Press [B] to display main menu" << endl;
-			cin >> back;
-			cout << endl;
-		} while (back == 'B' || back == 'b'); {
-			cout << "\n--------------------------------------------" << endl;
-			administratorMenu();
-		}
-	}
-	else if (soeChoice == 2)
-	{
-		cout << "1) edit" << endl;
-		cout << "2) delete" << endl;
-		cout << "3) add" << endl;
-		list.sort();
-		cin >> opt;
-		switch (opt) {
-		case 1: {
-			system("cls");
-			cout << "--------------------------------------" << endl;
-			cout << "Enter Order ID :" << endl;
-			cin >> id;
-			cout << "Enter Item ID :" << endl;
-			cin >> itId;
-			cout << "Enter Item Record :" << endl;
-			cin >> itRec;
-			cout << "Enter Item Brand :" << endl;
-			cin >> brand;
-			cout << "Enter Item price in ($):" << endl;
-			cin >> price;
-			list.edit(id, itId, itRec, brand, price, noItem);
-			cout << "Edited Successfully.." << endl;
-			cout << "--------------------------------------" << endl;
-			system("pause");
-			system("cls");
-			break;
-		}
-		case 2: {
-			system("cls");
-			cout << "--------------------------------------" << endl;
-			cout << "Enter the Order ID:";
-			cin >> orderDel;
-			list.deleteInBetween(orderDel);
-			cout << "Deleted Successfully.." << endl;
-			cout << "--------------------------------------" << endl;
-			system("pause");
-			system("cls");
-			break;
-		}
-		case 3: {
-			system("cls");
-			cout << "--------------------------------------" << endl;
-			cout << "Enter Order ID :" << endl;
-			cin >> id;
-			cout << "Enter Item ID :" << endl;
-			cin >> itId;
-			cout << "Enter Item Record :" << endl;
-			cin >> itRec;
-			cout << "Enter Item Brand :" << endl;
-			cin >> brand;
-			cout << "Enter Item price in ($):" << endl;
-			cin >> price;
-			list.insert(id, itId, itRec, brand, price, noItem);
-			cout << "Added Successfully.." << endl;
-			cout << "--------------------------------------" << endl;
-			system("pause");
-			system("cls");
-			break;
-		}
-		}
-	}
-	else if (soeChoice == 3)
-	{
-		do {
-			system("cls");
-			list.displayFromHead();
-			system("pause");
-			system("cls");
-			cout << "\n> Press [B] to display main menu" << endl;
-			cin >> back;
-			cout << endl;
-		} while (back == 'B' || back == 'b'); {
-			cout << "\n--------------------------------------------" << endl;
-			administratorMenu();
-		}
-	}
-	else if (soeChoice == 4)
-	{
-		/*sortOrder();*/
-	}
-	else if (soeChoice == 5)
-	{
-		/*searchOrder();*/
-	}
-	else if (soeChoice == 6)
-	{
-		main();
-	}
-}
-
-void administratorMenu()
-{
-	int aChoice, opt, noItem;
-	Orderlist list;
-	string id, itId, itRec, brand, price, orderDel;
-	char back;
-
-	cout << "1. Obtain purchase order records\n2. Modify/UpitemID order according to the priority" << endl;
-	cout << "3. View purchase orders\n4.Sort purchase orders\n5.Search purchase order\n6.View Sales Report\n7.View Summary Report\n8.Logout" << endl;
-	cin >> aChoice;
-	if (aChoice == 1)
-	{
-		do {
-			system("cls");
-			cout << "\n--------------------------------------------" << endl;
-			cout << "\n..................>>>>>>" << endl;
-			cout << "\nPurchase Order Records Obtained Succsefully..";
-			system("pause");
-			system("cls");
-			cout << "\n> Press [B] to display main menu" << endl;
-			cin >> back;
-			cout << endl;
-		} while (back == 'B' || back == 'b'); {
-			cout << "\n--------------------------------------------" << endl;
-			administratorMenu();
-		}
-	}
-	else if (aChoice == 2)
-	{
-		cout << "1) edit" << endl;
-		cout << "2) delete" << endl;
-		cout << "3) add" << endl;
-		list.sort();
-		cin >> opt;
-		switch (opt) {
-		case 1: {
-			system("cls");
-			cout << "--------------------------------------" << endl;
-			cout << "Enter Order ID :" << endl;
-			cin >> id;
-			cout << "Enter Item ID :" << endl;
-			cin >> itId;
-			cout << "Enter Item Record :" << endl;
-			cin >> itRec;
-			cout << "Enter Item Brand :" << endl;
-			cin >> brand;
-			cout << "Enter Item price in ($):" << endl;
-			cin >> price;
-			list.edit(id, itId, itRec, brand, price, noItem);
-			cout << "Edited Successfully.." << endl;
-			cout << "--------------------------------------" << endl;
-			system("pause");
-			system("cls");
-			break;
-		}
-		case 2: {
-			system("cls");
-			cout << "--------------------------------------" << endl;
-			cout << "Enter the Order ID:";
-			cin >> orderDel;
-			list.deleteInBetween(orderDel);
-			cout << "Deleted Successfully.." << endl;
-			cout << "--------------------------------------" << endl;
-			system("pause");
-			system("cls");
-			break;
-		}
-		case 3: {
-			system("cls");
-			cout << "--------------------------------------" << endl;
-			cout << "Enter Order ID :" << endl;
-			cin >> id;
-			cout << "Enter Item ID :" << endl;
-			cin >> itId;
-			cout << "Enter Item Record :" << endl;
-			cin >> itRec;
-			cout << "Enter Item Brand :" << endl;
-			cin >> brand;
-			cout << "Enter Item price in ($):" << endl;
-			cin >> price;
-			list.insert(id, itId, itRec, brand, price, noItem);
-			cout << "Added Successfully.." << endl;
-			cout << "--------------------------------------" << endl;
-			system("pause");
-			system("cls");
-			break;
-		}
-		}
-
-	}
-	else if (aChoice == 3)
-	{
-		do {
-			system("cls");
-			list.displayFromHead();
-			system("pause");
-			system("cls");
-			cout << "\n> Press [B] to display main menu" << endl;
-			cin >> back;
-			cout << endl;
-		} while (back == 'B' || back == 'b'); {
-			cout << "\n--------------------------------------------" << endl;
-			administratorMenu();
-		}
-	}
-	else if (aChoice == 4)
-	{
-		/*sortOrder();*/
-	}
-	else if (aChoice == 5)
-	{
-		/*searchOrder();*/
-	}
-	else if (aChoice == 6)// need to add status as extra parameter in Report
-	{
-		cout << "1) View in ascending Order" << endl;
-		cout << "2) View in descending Order" << endl;
-		list.sort();
-		cin >> opt;
-		switch (opt) {
-		case 1: {
-			do {
-				system("cls");
-				list.displayFromHead();
-				system("pause");
-				system("cls");
-				cout << "\n> Press [B] to display main menu" << endl;
-				cin >> back;
-				cout << endl;
-			} while (back == 'B' || back == 'b'); {
-				cout << "\n--------------------------------------------" << endl;
-				administratorMenu();
-				break;
-			}
-		}
-		case 2: {
-			do {
-				system("cls");
-				list.displayFromTail();
-				system("pause");
-				system("cls");
-				cout << "\n> Press [B] to display main menu" << endl;
-				cin >> back;
-				cout << endl;
-			} while (back == 'B' || back == 'b'); {
-				cout << "\n--------------------------------------------" << endl;
-				administratorMenu();
-				break;
-			}
-		}
-		}
-
-	}
-	else if (aChoice == 7)
-	{
-	cout << "1) View in ascending Order" << endl;
-	cout << "2) View in descending Order" << endl;
-	list.sort();
-	cin >> opt;
-	switch (opt) {
-	case 1: {
-		do {
-			system("cls");
-			list.displayHeadsummary();
-			system("pause");
-			system("cls");
-			cout << "\n> Press [B] to display main menu" << endl;
-			cin >> back;
-			cout << endl;
-		} while (back == 'B' || back == 'b'); {
-			cout << "\n--------------------------------------------" << endl;
-			administratorMenu();
-			break;
-		}
-	}
-	case 2: {
-		do {
-			system("cls");
-			list.displayTailsummary();
-			system("pause");
-			system("cls");
-			cout << "\n> Press [B] to display main menu" << endl;
-			cin >> back;
-			cout << endl;
-		} while (back == 'B' || back == 'b'); {
-			cout << "\n--------------------------------------------" << endl;
-			administratorMenu();
-			break;
-		}
-	}
-	}
-	}
-	else if (aChoice == 8)
-	{
-		main();
-	}
-}
+int Order::count = 0;
 
 int main()
 {
@@ -466,4 +157,305 @@ int main()
 		}
 	}
 	return 0;
+}
+void administratorMenu()
+{
+	int aChoice, opt, noItem;
+	Orderlist list;
+	string id, itId, itRec, brand, price, orderDel;
+	char back;
+	/*list.insert("001", "111", "Motherboard", "Asus", "780$", 10);
+	list.insert("002", "121", "Monitor", "Samsung", "344$", 100);
+	list.insert("003", "123", "Motherboard", "Dell", "123.46$", 50);
+	list.insert("004", "133", "CPU", "Intel", "24.60$", 25);
+	list.insert("005", "145", "Printer", "HP", "569$", 46);*/
+
+	cout << "1. Obtain purchase order records\n2. Modify/UpitemID order according to the priority" << endl;
+	cout << "3. View purchase orders\n4.Sort purchase orders\n5.Search purchase order\n6.View Sales Report\n7.View Summary Report\n8.Logout" << endl;
+	cin >> aChoice;
+	if (aChoice == 1)
+	{
+		do {
+			system("cls");
+			cout << "\n--------------------------------------------" << endl;
+			cout << "\n..................>>>>>>" << endl;
+			cout << "\nPurchase Order Records Obtained Succsefully..";
+			/*system("pause");
+			system("cls");*/
+			cout << "\n> Press [B] to display main menu" << endl;
+			cin >> back;
+			cout << endl;
+			system("cls");
+			administratorMenu();
+		} while (back == 'B' || back == 'b');
+	}
+	else if (aChoice == 2)
+	{
+		cout << "1) edit" << endl;
+		cout << "2) delete" << endl;
+		cout << "3) add" << endl;
+		list.sort();
+		cin >> opt;
+		switch (opt) {
+		case 1: {
+			system("cls");
+			cout << "--------------------------------------" << endl;
+			cout << "Enter Order ID :" << endl;
+			cin >> id;
+			cout << "Enter Item ID :" << endl;
+			cin >> itId;
+			cout << "Enter Item Record :" << endl;
+			cin >> itRec;
+			cout << "Enter Item Brand :" << endl;
+			cin >> brand;
+			cout << "Enter Item price in ($):" << endl;
+			cin >> price;
+			cout << "Enter No. of items :" << endl;
+			cin >> noItem;
+			list.edit(id, itId, itRec, brand, price, noItem);
+			cout << "Edited Successfully.." << endl;
+			cout << "--------------------------------------" << endl;
+			system("pause");
+			system("cls");
+			break;
+		}
+		case 2: {
+			system("cls");
+			cout << "--------------------------------------" << endl;
+			cout << "Enter the Order ID:";
+			cin >> orderDel;
+			list.deleteInBetween(orderDel);
+			cout << "Deleted Successfully.." << endl;
+			cout << "--------------------------------------" << endl;
+			system("pause");
+			system("cls");
+			break;
+		}
+		case 3: {
+			system("cls");
+			cout << "--------------------------------------" << endl;
+			cout << "Enter Order ID :" << endl;
+			cin >> id;
+			cout << "Enter Item ID :" << endl;
+			cin >> itId;
+			cout << "Enter Item Record :" << endl;
+			cin >> itRec;
+			cout << "Enter Item Brand :" << endl;
+			cin >> brand;
+			cout << "Enter Item price in ($):" << endl;
+			cin >> price;
+			cout << "Enter No. of items :" << endl;
+			cin >> noItem;
+			list.insert(id, itId, itRec, brand, price, noItem);
+			cout << "Added Successfully.." << endl;
+			cout << "--------------------------------------" << endl;
+			system("pause");
+			system("cls");
+			break;
+		}
+		}
+
+	}
+	else if (aChoice == 3)
+	{
+		do {
+			system("cls");
+			list.displayFromHead();
+			system("pause");
+			system("cls");
+			cout << "\n> Press [B] to display main menu" << endl;
+			cin >> back;
+			cout << endl;
+			system("cls");
+			administratorMenu();
+		} while (back == 'B' || back == 'b');
+	}
+	else if (aChoice == 4)
+	{
+		/*sortOrder();*/
+
+	}
+	else if (aChoice == 5)
+	{
+		/*searchOrder();*/
+	}
+	else if (aChoice == 6)// need to add status as extra parameter in Report
+	{
+		cout << "1) View in ascending Order based on No.Items" << endl;
+		cout << "2) View in descending Order based on No.Items" << endl;
+		list.sort();
+		cin >> opt;
+		switch (opt) {
+		case 1: {
+
+			system("cls");
+			list.displayFromHead();
+			cout << "\n--------------------------------------------" << endl;
+			system("pause");
+			system("cls");
+			administratorMenu();
+			break;
+
+		}
+		case 2: {
+			system("cls");
+			list.displayFromTail();
+			cout << "\n--------------------------------------------" << endl;
+			system("pause");
+			system("cls");
+			administratorMenu();
+			break;
+		}
+		}
+	}
+	else if (aChoice == 7)
+	{
+		cout << "1) View in ascending Order based on No.Items" << endl;
+		cout << "2) View in descending Order based on No.Items" << endl;
+		list.sort();
+		cin >> opt;
+		switch (opt) {
+		case 1: {
+
+			system("cls");
+			list.displayHeadsummary();
+			cout << "\n--------------------------------------------" << endl;
+			system("pause");
+			system("cls");
+			administratorMenu();
+			break;
+
+		}
+		case 2: {
+			system("cls");
+			list.displayTailsummary();
+			cout << "\n--------------------------------------------" << endl;
+			system("pause");
+			system("cls");
+			administratorMenu();
+			break;
+		}
+		}
+	}
+	else if (aChoice == 8)
+	{	
+		system("cls");
+		main();
+	}
+}
+void saleOrderExecutiveMenu()
+{
+	int soeChoice, opt, noItem;
+	Orderlist list;
+	string id, itId, itRec, brand, price, orderDel;
+	char back;
+	cout << "1. Obtain purchase order records\n2. Modify/UpitemID order according to the priority\n3. View purchase orders\n4. Sort purchase orders\n5. Search purchase order\n6. Logout" << endl;
+	cin >> soeChoice;
+	if (soeChoice == 1)
+	{
+		do {
+			system("cls");
+			cout << "\n--------------------------------------------" << endl;
+			cout << "\n..................>>>>>>" << endl;
+			cout << "\nPurchase Order Records Obtained Succsefully..";
+			cout << "\n--------------------------------------------" << endl;
+			/*system("pause");
+			system("cls");*/
+			cout << "\n> Press [B] to display main menu" << endl;
+			cin >> back;
+			administratorMenu();
+		} while (back == 'B' || back == 'b');
+	}
+	else if (soeChoice == 2)
+	{
+		cout << "1) edit" << endl;
+		cout << "2) delete" << endl;
+		cout << "3) add" << endl;
+		list.sort();
+		cin >> opt;
+		switch (opt) {
+		case 1: {
+			system("cls");
+			cout << "--------------------------------------" << endl;
+			cout << "Enter Order ID :" << endl;
+			cin >> id;
+			cout << "Enter Item ID :" << endl;
+			cin >> itId;
+			cout << "Enter Item Record :" << endl;
+			cin >> itRec;
+			cout << "Enter Item Brand :" << endl;
+			cin >> brand;
+			cout << "Enter Item price in ($):" << endl;
+			cin >> price;
+			cout << "Enter No. of items :" << endl;
+			cin >> noItem;
+			list.edit(id, itId, itRec, brand, price, noItem);
+			cout << "Edited Successfully.." << endl;
+			cout << "--------------------------------------" << endl;
+			system("pause");
+			system("cls");
+			break;
+		}
+		case 2: {
+			system("cls");
+			cout << "--------------------------------------" << endl;
+			cout << "Enter the Order ID:";
+			cin >> orderDel;
+			list.deleteInBetween(orderDel);
+			cout << "Deleted Successfully.." << endl;
+			cout << "--------------------------------------" << endl;
+			system("pause");
+			system("cls");
+			break;
+		}
+		case 3: {
+			system("cls");
+			cout << "--------------------------------------" << endl;
+			cout << "Enter Order ID :" << endl;
+			cin >> id;
+			cout << "Enter Item ID :" << endl;
+			cin >> itId;
+			cout << "Enter Item Record :" << endl;
+			cin >> itRec;
+			cout << "Enter Item Brand :" << endl;
+			cin >> brand;
+			cout << "Enter Item price in ($):" << endl;
+			cin >> price;
+			cout << "Enter No. of items :" << endl;
+			cin >> noItem;
+			list.insert(id, itId, itRec, brand, price, noItem);
+			cout << "Added Successfully.." << endl;
+			cout << "--------------------------------------" << endl;
+			system("pause");
+			system("cls");
+			break;
+		}
+		}
+	}
+	else if (soeChoice == 3)
+	{
+		do {
+			system("cls");
+			list.displayFromHead();
+			system("pause");
+			system("cls");
+			cout << "\n> Press [B] to display main menu" << endl;
+			cin >> back;
+			cout << endl;
+			system("cls");
+			administratorMenu();
+		} while (back == 'B' || back == 'b');
+	}
+	else if (soeChoice == 4)
+	{
+		/*sortOrder();*/
+	}
+	else if (soeChoice == 5)
+	{
+		/*searchOrder();*/
+	}
+	else if (soeChoice == 6)
+	{
+		main();
+	}
 }
--- Dstr/Report.cpp
+++ Dstr/Report.cpp
@@ -1,230 +1,230 @@
 #include "Report.h"
-Report::Report()
-{
-	head = nullptr;
-	tail = nullptr;
-}
-
-void Report::insert(string Id, string itId, string itR, string b, string p, int nIt)
-{
-	Order order(Id, itId, itR, b, p, nIt);
-	Node* newNode;
-	newNode = new Node;
-	newNode->next = nullptr;
-	newNode->prev = nullptr;
-	newNode->data = order;
-
-	if (head == nullptr)
-	{
-		head = newNode;
-		tail = newNode;
-	}
-	else
-	{
-		tail->next = newNode;
-		newNode->prev = tail;
-		tail = newNode; 
-	}
-}
-
-void Report::displayFromHead()
-{
-	Node* temp = head;
-	if (head == nullptr)
-	{
-		cout << "NO Orders To show.." << endl;
-	}
-	else
-	{
-		while (true)
-		{
-			temp->data.showOrderInfo();
-			cout << endl;
-			temp = temp->next;
-
-			if (temp == nullptr)
-				break;
-
-		}
-	}
-}
-void Report::displayHeadsummary()
-{
-	Node* temp = head;
-	if (head == nullptr)
-	{
-		cout << "NO Orders To show.." << endl;
-	}
-	else
-	{
-		while (true)
-		{
-			temp->data.showSummary();
-			cout << endl;
-			temp = temp->next;
-
-			if (temp == nullptr)
-				break;
-
-		}
-	}
-}
-
-void Report::displayFromTail()
-{
-	Node* temp = tail;
-	if (head == nullptr)
-	{
-		cout << "NO Orders To show.." << endl;
-	}
-	else
-	{
-		while (true)
-		{
-			temp->data.showOrderInfo();
-			cout << endl;
-			temp = temp->prev;
-
-			if (temp == nullptr)
-				break;
-
-		}
-	}
-}
-void Report::displayTailsummary()
-{
-	Node* temp = tail;
-	if (head == nullptr)
-	{
-		cout << "NO Orders To show.." << endl;
-	}
-	else
-	{
-		while (true)
-		{
-			temp->data.showSummary();
-			cout << endl;
-			temp = temp->prev;
-
-			if (temp == nullptr)
-				break;
-
-		}
-	}
-}
-void Report::sort() {
-	Node* current = NULL, * index = NULL;
-	Order temp;
-	 
-	if (head == NULL) {
-		cout << "NO Orders To show.." << endl;
-	}
-	else {
-		for (current = head; current->next != NULL; current = current->next) {
-			//Index will point to node next to current  
-			for (index = current->next; index != NULL; index = index->next) {
-				//comparing number of items if it greater than index  
-				if (current->data.NoItems > index->data.NoItems) {
-					temp = current->data;
-					current->data = index->data;
-					index->data = temp;
-				}
-			}
-		}
-	}
-}
-void Report::search(string d) //searching used here
-{
-	Node* temp = tail;
-	if (head == nullptr)
-	{
-		cout << "NO Orders To search.." << endl;
-	}
-	else {
-		while (true)
-		{
-			if (temp->data.itemID == d || temp->data.itemRecord == d || temp->data.Brand == d)
-			{
-				temp->data.showOrderInfo();
-
-				break;
-			}
-
-			else
-			{
-				temp = temp->prev;
-			}
-		}
-	}
-}
-void Report::edit(string Id, string itId, string itR, string b, string p, int nIt)
-{
-	Node* temp = tail;
-
-	while (true)
-	{
-		if (temp->data.orderID == Id)
-		{
-
-			temp->data.itemID = itId;
-			temp->data.itemRecord = itR;
-			temp->data.Brand = b;
-			temp->data.price = p;
-			temp->data.NoItems = nIt;
-			break;
-		}
-
-		else
-		{
-			temp = temp->prev;
-		}
-	}
-
-}
-void Report::deleteInBetween(string val)
-{
-	Node* temp = tail;
-	Node* temp1 = head;
-	if (head == nullptr)
-	{
-		cout << "NO Order To delete.." << endl;
-	}
-	else {
-		while (true)
-		{
-			if (temp1->prev || temp1->next == nullptr)
-			{
-				temp1->prev->next = nullptr;
-
-				delete temp1;
-				break;
-			}
-			if (temp->prev || temp->next == nullptr)
-			{
-				temp->prev->next = nullptr;
-
-				delete temp;
-				break;
-			}
-			if (temp->data.orderID == val)
-			{
-
-				temp->prev->next = temp->next;
-				temp->next->prev = temp->prev;
-				delete temp;
-				break;
-			}
-
-			else
-			{
-				temp = temp->prev;
-			}
-		}
-	}
-}
-
-Report::~Report()
-{
-
-
-}
+//Report::Report()
+//{
+//	head = nullptr;
+//	tail = nullptr;
+//}
+//
+//void Report::insert(string Id, string itId, string itR, string b, string p, int nIt)
+//{
+//	Order order(Id, itId, itR, b, p, nIt);
+//	Node* newNode;
+//	newNode = new Node;
+//	newNode->next = nullptr;
+//	newNode->prev = nullptr;
+//	newNode->data = order;
+//
+//	if (head == nullptr)
+//	{
+//		head = newNode;
+//		tail = newNode;
+//	}
+//	else
+//	{
+//		tail->next = newNode;
+//		newNode->prev = tail;
+//		tail = newNode; 
+//	}
+//}
+//
+//void Report::displayFromHead()
+//{
+//	Node* temp = head;
+//	if (head == nullptr)
+//	{
+//		cout << "NO Orders To show.." << endl;
+//	}
+//	else
+//	{
+//		while (true)
+//		{
+//			temp->data.showOrderInfo();
+//			cout << endl;
+//			temp = temp->next;
+//
+//			if (temp == nullptr)
+//				break;
+//
+//		}
+//	}
+//}
+//void Report::displayHeadsummary()
+//{
+//	Node* temp = head;
+//	if (head == nullptr)
+//	{
+//		cout << "NO Orders To show.." << endl;
+//	}
+//	else
+//	{
+//		while (true)
+//		{
+//			temp->data.showSummary();
+//			cout << endl;
+//			temp = temp->next;
+//
+//			if (temp == nullptr)
+//				break;
+//
+//		}
+//	}
+//}
+//
+//void Report::displayFromTail()
+//{
+//	Node* temp = tail;
+//	if (head == nullptr)
+//	{
+//		cout << "NO Orders To show.." << endl;
+//	}
+//	else
+//	{
+//		while (true)
+//		{
+//			temp->data.showOrderInfo();
+//			cout << endl;
+//			temp = temp->prev;
+//
+//			if (temp == nullptr)
+//				break;
+//
+//		}
+//	}
+//}
+//void Report::displayTailsummary()
+//{
+//	Node* temp = tail;
+//	if (head == nullptr)
+//	{
+//		cout << "NO Orders To show.." << endl;
+//	}
+//	else
+//	{
+//		while (true)
+//		{
+//			temp->data.showSummary();
+//			cout << endl;
+//			temp = temp->prev;
+//
+//			if (temp == nullptr)
+//				break;
+//
+//		}
+//	}
+//}
+//void Report::sort() {
+//	Node* current = NULL, * index = NULL;
+//	Order temp;
+//	 
+//	if (head == NULL) {
+//		cout << "NO Orders To show.." << endl;
+//	}
+//	else {
+//		for (current = head; current->next != NULL; current = current->next) {
+//			//Index will point to node next to current  
+//			for (index = current->next; index != NULL; index = index->next) {
+//				//comparing number of items if it greater than index  
+//				if (current->data.NoItems > index->data.NoItems) {
+//					temp = current->data;
+//					current->data = index->data;
+//					index->data = temp;
+//				}
+//			}
+//		}
+//	}
+//}
+//void Report::search(string d) //searching used here
+//{
+//	Node* temp = tail;
+//	if (head == nullptr)
+//	{
+//		cout << "NO Orders To search.." << endl;
+//	}
+//	else {
+//		while (true)
+//		{
+//			if (temp->data.itemID == d || temp->data.itemRecord == d || temp->data.Brand == d)
+//			{
+//				temp->data.showOrderInfo();
+//
+//				break;
+//			}
+//
+//			else
+//			{
+//				temp = temp->prev;
+//			}
+//		}
+//	}
+//}
+//void Report::edit(string Id, string itId, string itR, string b, string p, int nIt)
+//{
+//	Node* temp = tail;
+//
+//	while (true)
+//	{
+//		if (temp->data.orderID == Id)
+//		{
+//
+//			temp->data.itemID = itId;
+//			temp->data.itemRecord = itR;
+//			temp->data.Brand = b;
+//			temp->data.price = p;
+//			temp->data.NoItems = nIt;
+//			break;
+//		}
+//
+//		else
+//		{
+//			temp = temp->prev;
+//		}
+//	}
+//
+//}
+//void Report::deleteInBetween(string val)
+//{
+//	Node* temp = tail;
+//	Node* temp1 = head;
+//	if (head == nullptr)
+//	{
+//		cout << "NO Order To delete.." << endl;
+//	}
+//	else {
+//		while (true)
+//		{
+//			if (temp1->prev || temp1->next == nullptr)
+//			{
+//				temp1->prev->next = nullptr;
+//
+//				delete temp1;
+//				break;
+//			}
+//			if (temp->prev || temp->next == nullptr)
+//			{
+//				temp->prev->next = nullptr;
+//
+//				delete temp;
+//				break;
+//			}
+//			if (temp->data.orderID == val)
+//			{
+//
+//				temp->prev->next = temp->next;
+//				temp->next->prev = temp->prev;
+//				delete temp;
+//				break;
+//			}
+//
+//			else
+//			{
+//				temp = temp->prev;
+//			}
+//		}
+//	}
+//}
+//
+//Report::~Report()
+//{
+//
+//
+//}
